86d294921112bf2078918eaf7722fe463104f46e
lambrohan <lambrohan@gmail.com>
2021-12-22 10:33:12 +0530

    meta-updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,12 +4,17 @@ export default {
     host: '0.0.0.0',
   },
   head: {
-    title: 'salon-pwa',
+    title: 'My Salon - UBU',
 
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'UBU My Salon for salon owners and stylists. Manage your online bookings and walk in customers.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
